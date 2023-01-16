49d6f175c80db111b79acdb0580ec5ecf405bd5e
SebastianLenart <stentbike7@gmail.com>
2023-01-16 22:58:15 +0100

    16.01.2023

--- my_console/book.py
+++ my_console/book.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 
+
 class Book:
     def __init__(self, book_id: int, title: str, author: str, date_release, is_borrow: str, ):
         self.book_id = book_id,
         self.title = title,
-        self. author = author,
+        self.author = author,
         self.date_release = date_release,
-        self.is_borrowe = is_borrow
+        self.is_borrow = is_borrow
