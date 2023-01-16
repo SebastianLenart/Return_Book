18c8ea119ad9b1d404c8a5d70893a937dfa1c481
SebastianLenart <stentbike7@gmail.com>
2023-01-16 22:55:29 +0100

    16.01.2023

--- my_console/main.py
+++ my_console/main.py
@@ -8,14 +8,16 @@ from databasePS import Database
 class Menu():
     MENU = """---MENU---
     1.) List of books
-    2.) Add/Remove book
-    3.) List of borrowers
-    4.) Add/Remove borower
-    5.) Find a book by ..
-    6.) Find borower by ..
-    7.) Borrow a book
-    8.) Return a book
-    9.) Exit
+    2.) Add a book
+    3.) Remove a book
+    4.) List of borrowers
+    5.) Add a borrower
+    6.) Remove a borrower
+    7.) Find a book by ..
+    8.) Find borower by ..
+    9.) Borrow a book
+    10.) Return a book
+    11.) Exit
     Enter your choice: """
 
     def __init__(self):
@@ -26,11 +28,14 @@ class Menu():
     def list_of_books(self, db):
         pass
 
+    def add_book(self):
+        pass
+
     def start(self):
         with Database() as db:
             db.create_tables()
 
-        while (selection := input(self.MENU)) != "9":
+        while (selection := input(self.MENU)) != "11":
             try:
                 self.menu_options[selection](db)
             except KeyError:
